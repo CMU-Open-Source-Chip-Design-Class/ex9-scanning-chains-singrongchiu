995f964a872138de44867dd553e607524befaa7f
singrongchiu <singrongchiu@cmu.edu>
2025-03-29 17:56:32 -0400

    preliminary working scan chain and test for adder

--- ScanChain_starter.py
+++ ScanChain_starter.py
@@ -6,7 +6,7 @@ from cocotb.triggers import Timer
 # Make sure to set FILE_NAME
 # to the filepath of the .log
 # file you are working with
-CHAIN_LENGTH = -1
+CHAIN_LENGTH = 13 # need to change for each design
 FILE_NAME    = "adder/adder.log"
 
 
@@ -149,11 +149,14 @@ async def input_chain_single(dut, bit, ff_index):
     
     # ff is 0-indexed
     
+    dut.scan_en.value = 1
     dut.scan_in.value = bit
     await step_clock(dut)
     
     for i in range(ff_index):
       await step_clock(dut)
+      
+    dut.scan_en.value = 0
       
     return
     
@@ -172,13 +175,22 @@ async def input_chain(dut, bit_list, ff_index):
     # TODO: YOUR CODE HERE 
     ######################
     
-    dut.scan_in.value = bit
+    '''
+    dut.scan_in.value = bit_list[0]
     await step_clock(dut)
+    bit_list = bit_list[1:]
+    '''
     
-    for i in range(ff_index + 1):
+    dut.scan_en.value = 1
+    for i in range(len(bit_list)):
       dut.scan_in.value = bit_list[0]
       await step_clock(dut)
+      print(f"bit_list[0]: {bit_list[0]}")
       bit_list = bit_list[1:]
+    
+    for i in range(ff_index):
+      await step_clock(dut)
+    dut.scan_en.value = 0
       
     return
 
@@ -193,16 +205,19 @@ async def output_chain_single(dut, ff_index):
     # TODO: YOUR CODE HERE 
     ######################
     output = 0
+    
+    dut.scan_en.value = 1
     for i in range(CHAIN_LENGTH - ff_index):
       output = (output << 1) + dut.scan_out.value
       await step_clock(dut)
+    dut.scan_en.value = 0
     
     return output       
 
 #-----------------------------------------------
 
 # This function retrieves a single bit value from the
-# chain at specified index 
+# chain at specified index
 # This is an upgrade of input_chain_single() and should be accomplished
 #   for Part H of Task 1
         
@@ -211,13 +226,19 @@ async def output_chain(dut, ff_index, output_length):
     ######################
     # TODO: YOUR CODE HERE 
     ######################
+    
     output = 0
-    for i in range(CHAIN_LENGTH - ff_index):
+    print(f"first x_out {dut.x_out.value}")
+    
+    dut.scan_en.value = 1
+    for i in range(CHAIN_LENGTH - (ff_index + output_length)):
       await step_clock(dut)
+      print(f"x_out {dut.x_out.value}")
     
     for i in range(output_length): 
       output = (output << 1) + dut.scan_out.value
       await step_clock(dut)
+    dut.scan_en.value = 0
     
     return output
 
@@ -235,6 +256,7 @@ async def test(dut):
     # Setup the scan chain object
     chain = setup_chain(FILE_NAME)
     
+    '''
     print("PRINTING DUT")
     print(dut)
     print("PRINTING CHAIN")
@@ -245,8 +267,16 @@ async def test(dut):
       print(register[1].index_list)
       print(register[1].size)
     print(chain.chain_length)
-
-    ######################
-    # TODO: YOUR CODE HERE 
-    ######################
+    '''
+    
+    bit_list = [1, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0] # adding a = 10 and b = 7, initializing x_out to 0
+    # first 4 bits - a_reg, next 4 bits - b_reg, last 5 bits - x_out
+    await (input_chain(dut, bit_list, 0))
+    print(f"after input x_out {dut.x_out.value}")
+    # print(f"a_in {dut.a_in.value} b_in {dut.b_in.value}")
+    
+    await step_clock(dut)
+    outval = await (output_chain(dut, 0, 5))
+    print(f'this is what 10 + 7 is: {outval}')
+    
 
